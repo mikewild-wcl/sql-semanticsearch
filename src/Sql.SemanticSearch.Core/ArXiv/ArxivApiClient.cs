ed82b71abf46ce838186126d53e2e0174c496a7d
Mike Wild <wildconsultingltd@googlemail.com>
2026-01-20 12:14:08 +0000

    Start refactoring arxiv client and sql scripts
    ** not building **

--- src/Sql.SemanticSearch.Core/ArXiv/ArxivApiClient.cs
+++ src/Sql.SemanticSearch.Core/ArXiv/ArxivApiClient.cs
@@ -33,73 +33,32 @@ public class ArxivApiClient(
             "PDF downloaded successfully. Size: {PdfSize} bytes");
 
     [SuppressMessage("Usage", "CA2234:Pass system uri objects instead of strings", Justification = "The HttpClient will have a base uri set.")]
-    public async Task<ArxivPaper> GetPaperInfo(string arxivId)
+    public async IAsyncEnumerable<ArxivPaper> GetPaperInfo(IEnumerable<string> arxivIds, int maxItems = 10)
     {
-        try
+        //try
+        //{
+        //ArgumentException.ThrowIfNullOrEmpty(arxivId);
+        var start = 0;
+        do  //Loop will 
         {
-            ArgumentException.ThrowIfNullOrEmpty(arxivId);
-
-            // Clean the arXiv ID (remove version if present)
-            arxivId = arxivId.Replace("arXiv:", "", StringComparison.InvariantCultureIgnoreCase).Split('v')[0];
-
-            // Build the API query URL - we can do better and use a list, but need to be careful with max items
-            string queryUrl = $"query?id_list={arxivId}";
-
-            _logFetchingPaperInfo(_logger, queryUrl, null);
-
-            var response = await _httpClient.GetAsync(queryUrl);
-            response.EnsureSuccessStatusCode();
-
-            string xmlContent = await response.Content.ReadAsStringAsync();
-
-            var doc = XDocument.Parse(xmlContent);
-
-            // Define all relevant namespaces
-            XNamespace atom = "http://www.w3.org/2005/Atom";
-            XNamespace arxiv = "http://arxiv.org/schemas/atom";
-            XNamespace opensearch = "http://a9.com/-/spec/opensearch/1.1/";
-
-            var entry = doc.Descendants(atom + "entry").FirstOrDefault();
-            if (entry == null)
+            //Take maxItems
+            //Loop and retun
+            //Increment start
+            //Don't query more than max items. We aren't using arxiv paging in this code, just doing our own thing
+            foreach (var batch in arxivIds.Skip(start).Take(maxItems))
             {
-                throw new ArxivPaperNotFoundException(arxivId);
+                foreach (var item in batch)
+                {
+                    //why does the compiler think batch is string? ???
+                }
+                await foreach (var paper in GetPaperInfo(batch, start, maxItems))
+                {
+                    yield return paper;
+                }
+
+                start += maxItems;
             }
-
-            var entryId = entry.Element(atom + "id")?.Value;
-            var id = entryId.ToShortId();
-
-            var pdfLink = entry.Descendants(atom + "link")
-                .FirstOrDefault(l => l.Attribute("title")?.Value == "pdf");
-
-            var pdfUrl = pdfLink?.Attribute("href")?.Value;
-            var published = DateTime.TryParse(entry.Element(atom + "published")?.Value, out var publishedDate) ? publishedDate : DateTime.MinValue;
-
-            var paper = new ArxivPaper(id, entry.Element(atom + "title")?.Value?.Trim())
-            {
-                PdfUri = pdfUrl is not null ? new Uri(pdfUrl) : null,
-                Summary = entry.Element(atom + "summary")?.Value?.Trim() ?? string.Empty,
-                Comments = entry.Element(atom + "comment")?.Value?.Trim(),
-                Published = published,
-                Authors = entry.Descendants(atom + "author")
-                    .Select(a => a.Element(atom + "name")?.Value)
-                    .Where(name => !string.IsNullOrEmpty(name))
-                    .Select(name => name!)
-                    .ToList()
-                    ?? [],
-                Categories = entry.Descendants(atom + "category")
-                    .Select(c => c.Attribute("term")?.Value)
-                        .Where(term => !string.IsNullOrEmpty(term))
-                        .Select(term => term!)
-                        .ToList()
-                        ?? []
-            };
-
-            return paper;
-        }
-        catch (HttpRequestException ex)
-        {
-            throw new ArxivApiException($"Error fetching paper info: {ex.Message}", ex);
-        }
+        } while (start < maxItems);
     }
 
     public async Task<MemoryStream> DownloadPdfToMemoryStream(Uri pdfUri)
@@ -131,12 +90,92 @@ public class ArxivApiClient(
     {
         var paper = await GetPaperInfo(arxivId);
 
-        if(paper.PdfUri is null)
+        if (paper.PdfUri is null)
         {
             throw new ArxivPdfDownloadException("PDF URL is not available for this paper.");
         }
 
         var pdfStream = await DownloadPdfToMemoryStream(paper.PdfUri);
         return (paper, pdfStream);
+    }
+
+    private async IAsyncEnumerable<ArxivPaper> GetPaperInfo(IEnumerable<string> arxivIds, int start, int maxItems)
+    {
+        //try
+        //{
+        //ArgumentException.ThrowIfNullOrEmpty(arxivId);
+
+        /*
+         * Needs a loop that calls the rest of this code with start and maxResults until we have enough items or there are no more items.
+         https://info.arxiv.org/help/api/user-manual.html#paging
+         */
+
+        // Clean the arXiv ID (remove version if present)
+        var idList = arxivIds.ToList().ForEach(id => 
+            id.Replace("arXiv:", "", StringComparison.InvariantCultureIgnoreCase).Split('v')[0])
+            .Join(',');
+        //arxivId = arxivId.Replace("arXiv:", "", StringComparison.InvariantCultureIgnoreCase).Split('v')[0];
+
+        // Build the API query URL - we can do better and use a list, but need to be careful with max items
+        string queryUrl = $"query?id_list={idList}&start={start}&max_results={maxItems}";
+
+        _logFetchingPaperInfo(_logger, queryUrl, null);
+
+        var response = await _httpClient.GetAsync(queryUrl);
+        response.EnsureSuccessStatusCode();
+
+        string xmlContent = await response.Content.ReadAsStringAsync();
+
+        var doc = XDocument.Parse(xmlContent);
+
+        // Define all relevant namespaces
+        XNamespace atom = "http://www.w3.org/2005/Atom";
+        XNamespace arxiv = "http://arxiv.org/schemas/atom";
+        XNamespace opensearch = "http://a9.com/-/spec/opensearch/1.1/";
+
+        //var entry = doc.Descendants(atom + "entry").FirstOrDefault();
+        //if (entry == null)
+        //{
+        //    throw new ArxivPaperNotFoundException(arxivId);
+        //}
+
+        foreach (var entry = doc.Descendants(atom + "entry"))
+        {
+            var entryId = entry.Element(atom + "id")?.Value;
+            var id = entryId.ToShortId();
+
+            var pdfLink = entry.Descendants(atom + "link")
+                .FirstOrDefault(l => l.Attribute("title")?.Value == "pdf");
+
+            var pdfUrl = pdfLink?.Attribute("href")?.Value;
+            var published = DateTime.TryParse(entry.Element(atom + "published")?.Value, out var publishedDate) ? publishedDate : DateTime.MinValue;
+
+            var paper = new ArxivPaper(id, entry.Element(atom + "title")?.Value?.Trim())
+            {
+                PdfUri = pdfUrl is not null ? new Uri(pdfUrl) : null,
+                Summary = entry.Element(atom + "summary")?.Value?.Trim() ?? string.Empty,
+                Comments = entry.Element(atom + "comment")?.Value?.Trim(),
+                Published = published,
+                Authors = entry.Descendants(atom + "author")
+                    .Select(a => a.Element(atom + "name")?.Value)
+                    .Where(name => !string.IsNullOrEmpty(name))
+                    .Select(name => name!)
+                    .ToList()
+                    ?? [],
+                Categories = entry.Descendants(atom + "category")
+                    .Select(c => c.Attribute("term")?.Value)
+                        .Where(term => !string.IsNullOrEmpty(term))
+                        .Select(term => term!)
+                        .ToList()
+                        ?? []
+            };
+
+            yield return paper;
+        }
+        //}
+        //catch (HttpRequestException ex)
+        //{
+        //    throw new ArxivApiException($"Error fetching paper info: {ex.Message}", ex);
+        //}
     }
 }
